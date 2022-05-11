38aeb79af39e43d052132836be83c08c0198544f
Notiooo <dawidgrobert@gmail.com>
2022-05-12 00:39:53 +0200

    Add blobz, and html design with some initial view

--- PawMed/PawMed/settings.py
+++ PawMed/PawMed/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # Our apps
     'users',
+    'homepage',
 ]
 
 MIDDLEWARE = [
--- PawMed/PawMed/urls.py
+++ PawMed/PawMed/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
