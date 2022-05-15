1ddd31994cb2cf625ab49e2c2357a9911a3a34a8
Daniel Marek <56159259+danielmarek22@users.noreply.github.com>
2022-05-15 15:51:29 +0200

    Integrate existing database with Django - close #58
    * Add postgresql support to django project
    * Generate file with models for later use
    * Add database creation and deletion scripts
    * Add database creation instruction to readme file

--- PawMed/PawMed/settings.py
+++ PawMed/PawMed/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'PawMed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
     }
 }
 
