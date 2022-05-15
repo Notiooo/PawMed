9f3ada7ce849c99683249ab382b15d04c38a1ae5
Notiooo <dawidgrobert@gmail.com>
2022-05-15 19:55:52 +0200

    Fix templates & static folders location and search

--- PawMed/homepage/views.py
+++ PawMed/homepage/views.py
@@ -2,4 +2,5 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'homepage.html'
+    """Application main page (welcome page)"""
+    template_name = 'homepage/homepage.html'
