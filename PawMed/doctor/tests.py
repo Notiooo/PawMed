89e85b92a4810d9e2961986e743d63a0e81bfd14
danielmarek22 <danielmarek22@gmail.com>
2022-05-27 19:37:27 +0200

    Add custom test runner copatible with postgres db and test for doctor hompage view

--- PawMed/doctor/tests.py
+++ PawMed/doctor/tests.py
@@ -1,6 +1,19 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import Visit, Patient, Doctor
 # Create your tests here.
 
 class DoctorHomepageViewTest(TestCase):
-    
+
+    def testHomepageStatusCode(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def testHomepageUrlByName(self):
+        response = self.client.get(reverse('doctor_homepage'))
+        self.assertEqual(response.status_code, 200)
+
+    def testHomepageCorrectTemplate(self):
+        response = self.client.get(reverse('doctor_homepage'))
+        self.assertTemplateUsed(response, 'doctor/doctor_homepage.html')
+
