d5a9a184b2be67247eae64d2e61312abc3ff249e
Dawid Grobert <dawidgrobert@gmail.com>
2022-05-30 18:59:14 +0200

    Move some models to registrar

--- PawMed/doctor/admin.py
+++ PawMed/doctor/admin.py
@@ -1,22 +1,11 @@
 from django.contrib import admin
 
 from . import models
-# Register your models here.
 
 admin.site.register(models.Doctor)
-
 admin.site.register(models.Specialization)
-
 admin.site.register(models.DoctorSpecialization)
-
-admin.site.register(models.Patient)
-
-admin.site.register(models.Visit)
-
 admin.site.register(models.Prescription)
-
 admin.site.register(models.Test)
-
 admin.site.register(models.Laboratory)
-
 admin.site.register(models.Technician)
---
+++ PawMed/registrar/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from doctor.models import Doctor
+
+
+class Patient(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    registration_date = models.DateTimeField()
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=30)
+    birth_date = models.DateTimeField()
+    city = models.CharField(max_length=85)
+    zip_code = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    personid = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'patient'
+
+
+class Visit(models.Model):
+    id = models.IntegerField(primary_key=True)
+    doctor = models.ForeignKey(Doctor, models.DO_NOTHING, db_column='doctor')
+    patient = models.ForeignKey(Patient, models.DO_NOTHING, db_column='patient')
+    date = models.DateTimeField()
+    room = models.IntegerField()
+    remarks = models.TextField(blank=True, null=True)
+    diagnosis = models.TextField(blank=True, null=True)
+    medical_interview = models.TextField(blank=True, null=True)
+    examination = models.TextField(blank=True, null=True)
+    recommendation = models.TextField(blank=True, null=True)
+    took_place = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'visit'
