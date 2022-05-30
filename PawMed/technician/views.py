6d5454695beefc6b6b0106733e49dc31c4e4c108
JuliaBoczkowska <juliaboczkowska99@gmail.com>
2022-05-30 21:24:56 +0200

    Add design of head lab technician approval/control view - #33

--- PawMed/technician/views.py
+++ PawMed/technician/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 class SubmitResultView(TemplateView):
     """View where technician can add and then submit the result"""
     template_name = 'technician/submit_results.html'
+
+
+class HeadTechnicianApprovalView(TemplateView):
+    """View where technician can add and then submit the result"""
+    template_name = 'technician/approval_view.html'
