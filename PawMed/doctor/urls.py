713b09421c240ef1ee954678ad350f14661b5a22
danielmarek22 <danielmarek22@gmail.com>
2022-05-31 08:48:49 +0200

    Add doctor order test view

--- PawMed/doctor/urls.py
+++ PawMed/doctor/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.DoctorHomepageView.as_view(), name='doctor_homepage'),
     path('<int:pk>/end/', views.DoctorEndVisitView.as_view(), name='doctor_endvisit'),
     path('<int:pk>/visit/', views.DoctorAppointmentView.as_view(), name='doctor_visit'),
+    path('<int:pk>/test/', views.DoctorOrderTestView.as_view(), name='doctor_order_test'),
 ]
--- PawMed/doctor/views.py
+++ PawMed/doctor/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-
-from django.views.generic import ListView, DetailView, UpdateView
-
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.urls import reverse_lazy
 from datetime import datetime
 
 from . import models
@@ -38,6 +37,7 @@ class DoctorEndVisitView(UpdateView):
     model = models.Visit
     template_name = 'doctor/doctor_endvisit.html'
     fields=['tookplace']
+    success_url = reverse_lazy('doctor_homepage')
 
     # No way for getting back form this view now as get_absolute_url is not defined
     # but it makes the neccessary change in database
@@ -46,3 +46,23 @@ class DoctorAppointmentView(UpdateView):
     model = models.Visit
     template_name = 'doctor/doctor_visit.html'
     fields=['medical_interview', 'examination', 'remarks', 'recommendation', 'tookplace']
+    success_url = reverse_lazy('doctor_homepage')
+
+class DoctorOrderTestView(CreateView):
+    #There probably has to be some changes to test model
+    #for now execution date and executive are in the form
+    model = models.Test
+    template_name = 'doctor/doctor_order_test.html'
+    fields = ['type', 'remarks', 'execution_date', 'executive']
+    success_url = reverse_lazy('doctor_homepage')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.query = models.Visit.objects.get(pk=self.kwargs['pk'])
+        context['visit'] = self.query
+        return context
+
+    def form_valid(self, form):
+        form.instance.id = len(models.Test.objects.all())
+        form.instance.visit = models.Visit.objects.get(id=self.kwargs.get('pk'))
+        return super(DoctorOrderTestView, self).form_valid(form)
