0f6b2daac7eb825ecf7905eef4174301c69c545a
Dawid Grobert <dawidgrobert@gmail.com>
2022-06-04 00:32:02 +0200

    Add an administrator frontend and backend for assigning roles - close #85
    * Add an administator view and forms for assigning roles
    * Add frontend and backend mechanism for assigning roles to users
    * Add options header inside navbar dropdown
    * Add frontend for admin view
    * Add simple tests

--- PawMed/users/forms.py
+++ PawMed/users/forms.py
@@ -6,10 +6,16 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'role')
+        fields = ('username', 'email', 'first_name', 'last_name', 'phone_number')
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'phone_number')
+
+
+class CustomUserAssignRole(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('role', )
