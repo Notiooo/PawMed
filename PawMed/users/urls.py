bf005e4ae0973122e97a6f2051590a3f97f9b822
Dawid Grobert <dawidgrobert@gmail.com>
2022-06-19 20:52:01 +0200

    Add doctors management page for admin - close #98
    * Add panel for adding specializations and editing data of doctors
    * Add tests and some improvements
    
    Co-authored-by: JuliaBoczkowska <juliaboczkowska99@gmail.com>

--- PawMed/users/urls.py
+++ PawMed/users/urls.py
@@ -9,5 +9,13 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('no_role/', views.NoRoleView.as_view(), name='no_role'),
     path('admin/', views.AdminAssigningRolesListView.as_view(), name='admin_roles'),
-    path('admin/<int:pk>/', views.AdminAssigningRolesUpdateView.as_view(), name='admin_roles_update')
+    path('admin-doctor/', views.AdminDoctorListView.as_view(), name='admin_doctors'),
+    path('admin/<int:pk>/', views.AdminAssigningRolesUpdateView.as_view(), name='admin_roles_update'),
+    path('admin-doctor-update/<int:pk>/', views.AdminDoctorUpdateView.as_view(), name='admin_doctor_update'),
+    path('admin-doctor-specialization-update/<int:pk>/', views.AdminDoctorSpecializationUpdateView.as_view(),
+         name='admin_doctor_specialization_update'),
+    path('ajax/delete-doctor-specialization/<int:doctor_specialization_id>/', views.DeleteDoctorSpecializationView.as_view(),
+         name="ajax_doctor_specialization_delete"),
+    path('ajax/add-doctor-specialization/<int:specialization_id>/<int:doctor_id>/', views.AddDoctorSpecializationView.as_view(),
+         name="ajax_doctor_specialization_add")
 ]
